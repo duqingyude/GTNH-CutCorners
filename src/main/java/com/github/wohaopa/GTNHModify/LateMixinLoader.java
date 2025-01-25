f77209f697c96858c19bda01140ee18fba0a1e10
Taskeren <r0yalist@outlook.com>
2025-01-26 00:32:29 +0800

    fix #11 & update buildscript

--- src/main/java/com/github/wohaopa/GTNHModify/LateMixinLoader.java
+++ src/main/java/com/github/wohaopa/GTNHModify/LateMixinLoader.java
@@ -31,6 +31,7 @@ public class LateMixinLoader implements ILateMixinLoader {
             mixins.add("gregtech.GT_MetaTileEntity_MultiFurnaceMixin");
             mixins.add("gregtech.GT_MetaTileEntity_DrillerBaseMixin");
             mixins.add("gregtech.EyeOfHarmonyRecipeAccessor");
+            mixins.add("gregtech.EyeOfHarmonyFrontendFixMixin");
         }
         if (Mods.Thaumcraft.isLoaded()) {
             mixins.add("thaumcraft.TileAlchemyFurnaceMixin");
---
+++ src/main/java/com/github/wohaopa/GTNHModify/mixins/late/gregtech/EyeOfHarmonyFrontendFixMixin.java
@@ -0,0 +1,23 @@
+package com.github.wohaopa.GTNHModify.mixins.late.gregtech;
+
+import gregtech.api.util.GTUtility;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import tectech.util.TTUtility;
+
+@Mixin(targets = {"tectech.recipe.EyeOfHarmonyFrontend$EyeOfHarmonySpecialValueFormatter"}, remap = false)
+public class EyeOfHarmonyFrontendFixMixin {
+
+    @Redirect(method = "format", at = @At(value = "INVOKE", target = "Ltectech/util/TTUtility;toExponentForm(J)Ljava/lang/String;"))
+    private String toExponentForm_crashFix(long number) {
+        if(number > 1000) {
+            // this function will crash the game if the number is less than 1,000,
+            // so we should use normal one instead if it happens.
+            return TTUtility.toExponentForm(number);
+        } else {
+            return GTUtility.formatNumbers(number);
+        }
+    }
+
+}
